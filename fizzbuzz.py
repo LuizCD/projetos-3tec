2776e4aeb1e6d4b9a090b9685f8a5775e31dc8c1
luizcd <luizcdprof@gmail.com>
2024-06-12 12:31:29 -0300

    templates

--- fizzbuzz.py
+++
@@ -1,9 +0,0 @@
-def fizzbuzz(num:int):
-    if num % 3 == 0:
-        return 'fizz'    
-    elif num % 5 == 0:
-        return 'buzz'
-    elif num % 3 == 0 and num % 5 == 0:
-        return 'fizzbuzz'
-    else:
-        return num
